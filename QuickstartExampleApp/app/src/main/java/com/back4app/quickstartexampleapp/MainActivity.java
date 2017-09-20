446f11c669946cba772bca3f413f05dbd6fd8f40
senaingrid <ingrid@back4app.com>
2017-09-20 19:50:24 -0300

    Step 2 - Configuring App Permissions and Libs

--- QuickstartExampleApp/app/src/main/java/com/back4app/quickstartexampleapp/MainActivity.java
+++ QuickstartExampleApp/app/src/main/java/com/back4app/quickstartexampleapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.back4app.quickstartexampleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
