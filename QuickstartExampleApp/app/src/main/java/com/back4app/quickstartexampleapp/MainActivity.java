d2a55426840caf1c2ce0d7d929e0824dd2c1f3d5
senaingrid <ingrid@back4app.com>
2017-09-20 19:52:54 -0300

    Step 3 - Connecting your Parse App

--- QuickstartExampleApp/app/src/main/java/com/back4app/quickstartexampleapp/MainActivity.java
+++ QuickstartExampleApp/app/src/main/java/com/back4app/quickstartexampleapp/MainActivity.java
@@ -12,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("8cQOhBQXiYrv5cnxVqRbUBJ53KfDIqHojw3U1N7s") //PASTE YOUR Back4App APPLICATION ID
+                .clientKey("Dt0cVr0m9TD5MY3CC85JrnwmP33fJUJI90lM6Jny") //PASTE YOUR CLIENT KEY
+                .server("https://parseapi.back4app.com/").build()
+        );
     }
 }
