097a9305c928af07fa29abce51ecc161d08e9ed6
Ingrid Sena <ingrid@back4app.com>
2017-10-17 13:20:50 -0200

    Updating strings config

--- app/src/main/java/com/back4app/quickstartexampleapp/MainActivity.java
+++ app/src/main/java/com/back4app/quickstartexampleapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.back4app.quickstartexampleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import com.parse.Parse;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
@@ -12,10 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("8cQOhBQXiYrv5cnxVqRbUBJ53KfDIqHojw3U1N7s") //PASTE YOUR Back4App APPLICATION ID
-                .clientKey("Dt0cVr0m9TD5MY3CC85JrnwmP33fJUJI90lM6Jny") //PASTE YOUR CLIENT KEY
-                .server("https://parseapi.back4app.com/").build()
-        );
+
+        Parse.initialize(this);
+
     }
+
 }
