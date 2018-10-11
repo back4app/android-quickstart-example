b6121c1ed466780fceb85bbc48ad607544dafde5
thay_baldao <thayna.baldao@gmail.com>
2018-10-11 16:48:34 -0300

    updating to newest parse sdk

--- app/src/main/java/com/back4app/quickstartexampleapp/App.java
+++ app/src/main/java/com/back4app/quickstartexampleapp/App.java
@@ -8,6 +8,12 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Parse.initialize(this);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                // if defined
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build()
+        );
     }
 }
--- app/src/main/java/com/back4app/quickstartexampleapp/MainActivity.java
+++ app/src/main/java/com/back4app/quickstartexampleapp/MainActivity.java
@@ -3,10 +3,6 @@ package com.back4app.quickstartexampleapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.parse.Parse;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseUser;
 import com.parse.ParseInstallation;
 
 public class MainActivity extends AppCompatActivity {
